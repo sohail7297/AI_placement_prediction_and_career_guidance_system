cad03c4903e07fa17cc03b14402c5fd35ea1599b
Sohail Khan <sohailkhan74336@gmail.com>
2026-07-10 11:07:14 +0530

    aaj bhi nhi hua

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import MainLayout from "../layouts/MainLayout";
+import Hero from "../components/home/Hero";
+
+function Home() {
+  return (
+    <MainLayout>
+
+      <Hero/>
+
+    </MainLayout>
+  );
+}
+
+export default Home;
